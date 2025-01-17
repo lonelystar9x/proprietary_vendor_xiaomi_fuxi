1e9e3216f92d4de430ac2fef055e2c6a22aa95ea
lollipuru <banstellazz@gmail.com>
2025-01-17 19:04:42 +0000

    fuxi: initial device specific libs from OS2.0.3.0.VMCCNXM
    Signed-off-by: lollipuru <banstellazz@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),fuxi)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,00c11fe853c0f9a8c2737b106c53852f6c156d6d)
+$(call add-radio-file-sha1-checked,radio/aop.img,7b0be2ff860bcb0843d9a008d1af793ebb0cad39)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,bb0b2b1e354228702de2a395228abdae8d2c5a9e)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,d3a8e1092a0b93c18b7c36a4009050c14a5fc6f8)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,96b5e7731c065da7c849903913c3dd7073a9ce92)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,046194dfb6eb5f2b7889c98e10469feab9f0501b)
+$(call add-radio-file-sha1-checked,radio/dsp.img,a77bd1a305e2558f8c35fa38ff9a370d20024efb)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,48e36dc7b80d6ff1fbae37ab8cd657c75c8f8440)
+$(call add-radio-file-sha1-checked,radio/hyp.img,358110feaf3b5bd33ff0f707a9cea1f4da5d7fe3)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,18752b2f1675a5c6587f05158dad47002bc9a965)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,3d1be46d76337b95c2d1abd438bbf026d728db53)
+$(call add-radio-file-sha1-checked,radio/modem.img,5bcfb5d49e30ed7a0317469faeed93688df3905a)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,354e6afcde18558dedfa0a0b88512e166b4f5ea5)
+$(call add-radio-file-sha1-checked,radio/shrm.img,222623041a6a8f0f898cb4c753d2ed392672e984)
+$(call add-radio-file-sha1-checked,radio/tz.img,a432b38ef17986e1dd86b6dd7f35b2c063cd1d0f)
+$(call add-radio-file-sha1-checked,radio/uefi.img,ca5700dd284ca5568b0f845f4ee3ddc831fadc43)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,d7fcaf78af55f8d542e6fe1405d49d1746eff225)
+$(call add-radio-file-sha1-checked,radio/xbl.img,43c118a357ef438804b0f6730deb7e5ac09630df)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,5191781a86c6d4d6184e10a3acd365ef8688c3b8)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,b1c20059c15688e20394a127ab5bb987bc7c4c9c)
+
+endif
